946cc5151a871a56e2702e8cca4e58b1b730324e
Xanderk <Xanderkidwell@gmail.com>
2022-09-26 15:26:16 -0700

    commit tree fae374f73b25ecf32df078f43b866a34cc392d4d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(record) {
+    let foundObject = record.find((object) => {
+        if (object.result === "W") return object.year
+    })
+    if (foundObject === undefined) {
+        return foundObject
+    } else {
+        return foundObject.year
+
+    }
+
+}
